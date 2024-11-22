caea9b9e5f68f1f07b1e9badd7dfc1e2e3e63090
Daniel Ford <forddj93@gmail.com>
2024-11-22 12:09:29 +0000

    Image uploading working, going to clean up implementations now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { Button } from "@/components/ui/button";
+    import { Button, buttonVariants } from "@/components/ui/button";
     import { Textarea } from "@/components/ui/textarea";
     import { invoke } from "@tauri-apps/api/core";
     import { ImagePlus, Loader2 } from "lucide-svelte";
@@ -12,9 +12,12 @@
         PUBLIC_TWITTER_ACCESS_TOKEN,
         PUBLIC_TWITTER_ACCESS_TOKEN_SECRET,
     } from "$env/static/public";
+    import { Label } from "@/components/ui/label";
 
     let message = $state("");
     let sending = $state(false);
+
+    let files = $state<File[]>([]);
 
     const bskyAgent = new BskyAgent({
         service: "https://bsky.social",
@@ -31,6 +34,17 @@
     }
 
     const sendToBsky = async () => {
+        let uploads = [];
+
+        if (files?.length > 0) {
+            for (let i = 0; i < files.length; i++) {
+                uploads.push({
+                    image: (await bskyAgent.uploadBlob(files[i], { encoding: "image/png" })).data.blob,
+                    alt: "",
+                });
+            }
+        }
+
         // creating richtext
         const rt = new RichText({
             text: message,
@@ -42,19 +56,40 @@
             text: rt.text,
             facets: rt.facets,
             createdAt: new Date().toISOString(),
+            embed: {
+                images: uploads,
+                $type: "app.bsky.embed.images",
+            },
         };
 
         await bskyAgent.post(postRecord);
     };
 
     const sentToTwitterX = async () => {
+        // Convert files to array buffer
+        const filesData = await Promise.all(
+            files.map(async (file) => {
+                const arrayBuffer = await file.arrayBuffer();
+                return {
+                    buffer: Array.from(new Uint8Array(arrayBuffer)),
+                    type: file.type
+                };
+            })
+        );
+
         await fetch("/api/twitter", {
             method: "POST",
-            body: JSON.stringify({ message }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ 
+                message, 
+                files: filesData 
+            }),
         });
     };
 
-    async function greet(event: Event) {
+    const send = async (event: Event) => {
         event.preventDefault();
 
         if (sending) {
@@ -70,17 +105,27 @@
         message = "";
 
         sending = false;
-    }
+    };
+
+    const addFiles = (event: Event) => {
+        const formFiles = event.target.files;
+
+        // Loop over the selected files and append them to the array
+        for (let i = 0; i < formFiles.length; i++) {
+            files.push(formFiles[i]);
+        }
+    };
 </script>
 
 <div class="container flex flex-col items-center justify-center h-screen">
-    <form class="w-[500px] flex flex-col gap-4 py-4" onsubmit={greet}>
+    <form class="w-full max-w-[600px] flex flex-col gap-4 py-4" onsubmit={send}>
         <Textarea bind:value={message} placeholder="Post something..." class="min-h-[100px]" />
 
         <div class="flex justify-end gap-2">
-            <Button variant="outline">
+            <Label for="file" class={buttonVariants({ variant: "outline" })}>
                 <ImagePlus />
-            </Button>
+            </Label>
+            <input multiple accept="image/png, image/jpeg" class="hidden" id="file" name="file" type="file" onchange={addFiles} />
             <Button class="grow" type="submit" disabled={sending}>
                 {#if sending}
                     <Loader2 class="animate-spin" />
@@ -88,6 +133,20 @@
                     Send
                 {/if}
             </Button>
+        </div>
+        <div class="grid grid-cols-4 gap-2">
+            {#if files && files.length > 0}
+                {#each files as image, index}
+                    <button class="w-full h-[200px] bg-gray-200 rounded-md overflow-hidden group relative" onclick={() => files.splice(index, 1)}>
+                        <div
+                            class="absolute top-0 right-0 w-full h-full opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-black/50 text-white flex items-center justify-center"
+                        >
+                            Remove File
+                        </div>
+                        <img src={URL.createObjectURL(image)} alt="uploaded image" class="object-cover w-full h-full" />
+                    </button>
+                {/each}
+            {/if}
         </div>
     </form>
 </div>
--- src/routes/api/twitter/+server.ts
+++ src/routes/api/twitter/+server.ts
@@ -1,18 +1,40 @@
 import { PUBLIC_TWITTER_ACCESS_TOKEN, PUBLIC_TWITTER_ACCESS_TOKEN_SECRET, PUBLIC_TWITTER_API_KEY, PUBLIC_TWITTER_API_KEY_SECRET, PUBLIC_TWITTER_CLIENT_ID, PUBLIC_TWITTER_CLIENT_SECRET } from "$env/static/public";
+import { json } from "@sveltejs/kit";
 import { TwitterApi } from "twitter-api-v2";
+import { Buffer } from 'buffer';
+
+let twitterClient = new TwitterApi({
+    appKey: PUBLIC_TWITTER_API_KEY,
+    appSecret: PUBLIC_TWITTER_API_KEY_SECRET,
+    accessToken: PUBLIC_TWITTER_ACCESS_TOKEN,
+    accessSecret: PUBLIC_TWITTER_ACCESS_TOKEN_SECRET
+});
+
 
 
 export async function POST({ request }) {
-    const { message } = await request.json();
+    const { message, files } = await request.json();
 
-    let twitterClient = new TwitterApi({
-        appKey: PUBLIC_TWITTER_API_KEY,
-        appSecret: PUBLIC_TWITTER_API_KEY_SECRET,
-        accessToken: PUBLIC_TWITTER_ACCESS_TOKEN,
-        accessSecret: PUBLIC_TWITTER_ACCESS_TOKEN_SECRET
-    });
+    let mediaIds = [];
+    
+    // Convert array buffer to Buffer and upload
+    if (files && files.length > 0) {
+        for (const file of files) {
+            // Convert the array back to Buffer
+            const buffer = Buffer.from(file.buffer);
+            const mediaId = await twitterClient.v1.uploadMedia(buffer, { mimeType: file.type });
+            mediaIds.push(mediaId);
+        }
+    }
 
-    const tweet = await twitterClient.v2.tweet(message);
+    const tweet = await twitterClient.v2.tweet({
+        text: message,
+        media:  {
+            media_ids: mediaIds.length > 0 ? mediaIds : undefined,
+            tagged_user_ids: []
+        }
+        
+    }); 
 
-    return new Response(JSON.stringify(tweet), { status: 200 });
+    return json(tweet);
 }
